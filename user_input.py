910c8590971fa291fccb9f174b3cb14e9180a01a
LALIT WAZIR <wazirlalit96420@gmail.com>
2021-02-21 20:39:37 +0530

    Python cheet Sheet and Syntax guide

---
+++ user_input.py
@@ -0,0 +1,22 @@
+user_input = input("Please provide the input....")
+print(user_input)
+
+
+user_no = 10;
+a=b=c=10
+print(a)
+print(b)
+print(c)
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+a=10
+print(id(10))
+print(id(a))
+b=a
+print(id(b))
+
+b=20
+id(b)
+print(id(b))
